13ecfcda05d029aa7cd15844da6bdc69c291e56a
Diego P. Dobniewski <ddobniewski@ign.gob.ar>
2019-10-01 16:03:50 -0300

    Agregado estadisticas

---
+++ js/analytics/analytics.js
@@ -0,0 +1,7 @@
+$.getScript("https://www.googletagmanager.com/gtag/js?id=XX-NNNNNNNNN-N", function( data, textStatus, jqxhr ) {
+	window.dataLayer = window.dataLayer || [];			
+	function gtag(){dataLayer.push(arguments);
+    gtag('js', new Date());
+    gtag('config', 'XX-NNNNNNNNN-N');	
+}		
+	
