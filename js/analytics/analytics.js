a2cdc3dda7d87a214875c72a2c5ae3ac5c81c1ec
andreazomoza <aszomoza@gmail.com>
2020-12-18 11:38:01 -0300

    ga4

--- js/analytics/analytics.js
+++ js/analytics/analytics.js
@@ -4,4 +4,10 @@ $.getScript("https://www.googletagmanager.com/gtag/js?id=UA-136146579-6", functi
     gtag('js', new Date());
     gtag('config', 'UA-136146579-6');
 });
-	
+
+$.getScript("https://www.googletagmanager.com/gtag/js?id=G-D28Z64ZEDD", function( data, textStatus, jqxhr ) {
+	window.dataLayer = window.dataLayer || [];			
+	function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', 'G-D28Z64ZEDD');
+});
